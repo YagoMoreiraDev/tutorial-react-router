afd382b3f28e6cb6596ae1e29bb8554b6e17c6a8
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-08-31 14:25:08 -0300

    Adc a tag BrowserRouter e Link para navegar pelos links sem carregar a pag toda

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <h1>Bookkeeper!</h1>
+    <BrowserRouter>
+      <div>
+        <h1>Bookkeeper!</h1>
+        <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      </div>
+    </BrowserRouter>
+    
   )
 }
 
