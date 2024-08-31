d9850429119d88db28a319dc8246da67cc59158b
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-08-31 14:55:18 -0300

    Adc o elemento Routes | Route para trabalhar com rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <h1>Bookkeeper!</h1>
-        <nav
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
@@ -15,10 +14,8 @@ function App() {
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
-      </div>
-    </BrowserRouter>
-    
-  )
+    </div>
+  );
 }
 
 export default App
